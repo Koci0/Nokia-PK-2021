9443add94c2da53d558334052baa7d465ca6b27b
jakjare <jakjare@wp.pl>
2021-05-05 14:56:37 +0200

    Sending Call Request: alternative failure scenario - timeout. Timeout implementation in TimerPort.

--- UE/Application/Ports/TimerPort.cpp
+++ UE/Application/Ports/TimerPort.cpp
@@ -22,11 +22,28 @@ void TimerPort::stop()
 void TimerPort::startTimer(Duration duration)
 {
     logger.logDebug("Start timer: ", duration.count(), "ms");
+    this->running = true;
+    this->timer = std::async(&TimerPort::timerThread, this, duration);
 }
 
 void TimerPort::stopTimer()
 {
     logger.logDebug("Stop timer");
+    this->running = false;
+    this->timer.get();
+}
+
+void TimerPort::timerThread(Duration duration)
+{
+    using namespace std::chrono_literals;
+    for(int i = duration / 100ms; i > -1; i--)
+    {
+        std::this_thread::sleep_for(100ms);
+        if (!this->running)
+            return;
+    }
+    logger.logError("Timeout");
+    handler->handleTimeout();
 }
 
 }
