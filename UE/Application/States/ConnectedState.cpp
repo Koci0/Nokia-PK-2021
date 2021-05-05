2d8c1bc48693fb591d0271b2d45ec4ebb515c6ba
jakjare <jakjare@wp.pl>
2021-05-05 16:15:13 +0200

    Sending Call Request: alternative failure scenario - timeout completed.

--- UE/Application/States/ConnectedState.cpp
+++ UE/Application/States/ConnectedState.cpp
@@ -18,18 +18,28 @@ void ConnectedState::handleDisconnected()
 void ConnectedState::handleSendCallRequest(common::PhoneNumber to)
 {
     context.bts.sendCallRequest(to);
+    using namespace std::chrono_literals;
+    context.timer.startTimer(60s);
 }
 
 void ConnectedState::handleCallAccepted()
 {
     // TODO change mode to talking state
     logger.logInfo("ConnectedState: handleCallAccepted");
+    context.timer.stopTimer();
 }
 
 void ConnectedState::handleCallDropped()
 {
     logger.logInfo("ConnectedState: handleCallDropped");
     context.user.showShortInfo("User dropped a call.");
+    context.timer.stopTimer();
+}
+
+void ConnectedState::handleTimeout()
+{
+    logger.logInfo("Timeout");
+    context.user.showShortInfo("Timeout.");
 }
 
 }
