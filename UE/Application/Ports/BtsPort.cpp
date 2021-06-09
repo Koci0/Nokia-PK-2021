b8e7f6bac87ca66836c3cbc6f4e295a13730b45e
KazdMomi <mikwiecinski99@wp.pl>
2021-06-09 18:26:38 +0200

    Test

--- UE/Application/Ports/BtsPort.cpp
+++ UE/Application/Ports/BtsPort.cpp
@@ -6,7 +6,6 @@
 namespace ue
 {
 
-
 BtsPort::BtsPort(common::ILogger &logger, common::ITransport &transport, common::PhoneNumber phoneNumber)
     : logger(logger, "[BTS-PORT]"),
       transport(transport),
